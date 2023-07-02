60ba6d45d2c14204e49aad168c90e8a7129e7bfd
Mikael Lind <elemel@elemel.se>
2023-07-02 09:24:56 +0200

    Add optional entity parameter

--- sparrow/Database.lua
+++ sparrow/Database.lua
@@ -54,8 +54,15 @@ function M:containsRow(entity)
   return self._archetypes[entity] ~= nil
 end
 
-function M:insertRow(cells)
-  local entity = self._maxEntity + 1
+function M:insertRow(cells, entity)
+  if entity then
+    if self._archetypes[entity] then
+      error("Duplicate row: " .. entity)
+    end
+  else
+    entity = self._maxEntity + 1
+  end
+
   self._maxEntity = entity
 
   self._archetypes[entity] = {}
